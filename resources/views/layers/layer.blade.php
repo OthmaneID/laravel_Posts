c1fccbf977b16d15d78a02100ac82f875d6f24e3
OthmaneID <othmanid5@users.noreply.github.com>
2022-01-04 22:06:56 +0100

    beginning design commit

---
+++ resources/views/layers/layer.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 	integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Posts app</title>
+</head>
+<body class="bg-gray-200">
+    <nav class="nav text-3xl text-light underline bg-blue-500 rounded-b-lg py-3" >
+        <p>Posts Web App</p> 
+        <div class="nav-icons" >
+            <a href="">
+                <i></i>
+            </a>
+        </div>
+    </nav>
+    <div class="container pt-2 position-relative">
+        @yield('content')
+    </div>
+    
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layers.layer')
+
+
+@section('content') 
+    <div class="d-flex align-items-center justify-content-center">
+        
+        <div class="left-sidebar position-absolute left-0 top-0 bottom-0 border
+            w-auto ">
+
+        </div>
+
+        <div class="post w-1/3 bg-white rounded-lg text-center " >
+            <h2 class="text-xl py-3" >post</h2>  
+        </div>
+
+        <div class="right-sidebar">
+
+        </div>
+       
+    </div>
+@endsection
